bb8814d9e8e08e6d1c4d7a61ae0903e9ad4afe9a
leanhvu1509 <vuanhle1509@gmail.com>
2021-10-21 12:16:18 +0700

    B2 - Hoàn thiện hệ thống (menu và 1 số chức năng nhỏ)

--- library.py
+++ library.py
@@ -58,9 +58,10 @@ class Library:
     def sua_sach(self):
         idsach = input("Nhập ID sách: ")
         if idsach in self.dictBook.keys():
-            if not self.dictBook[idsach]['status'] == 'Có sẵn' :
+    
+            if not self.dictBook[idsach]['status'] == "Có sẵn" :
                 print(f"Sách này đang được thuê vui lòng nên chưa cập nhật được!!")
-            elif self.dictBook[idsach]['status'] == 'Có sẵn' :
+            elif self.dictBook[idsach]['status'] == "Có sẵn" :
                 nhap_sach = input("Nhập tên: ")
                 if nhap_sach == "" :
                     print("Lỗi tên sách không được để trống. Vui lòng làm lại!!")
@@ -69,8 +70,13 @@ class Library:
                     print("Lỗi tiêu đề sách quá dài (quá 30 chữ). Vui lòng làm lại!!")
                     return self.sua_sach()
                 else:
-                    # with open(self.list_books,"a",encoding="utf8") as ub:
+                    # with open("listbook.txt","r",encoding="utf8") as lbss:
+                    #     lbooksc = lbss.readlines()
+                    # lbss.close()
+                    # ub = open(self.list_books,"a",encoding="utf8")
+                    # for line in ub:
                     #     ub.writelines(f"{nhap_sach}\n")
+
                     self.dictBook[idsach]['ten_sach'] = nhap_sach
                     print("Đã thuê thành công!\n")
         else:
@@ -80,31 +86,55 @@ class Library:
     def xoa_sach(self):
         idsach = input("Nhập ID sách: ")
         if idsach in self.dictBook.keys():
-            pass
+            del self.dictBook[idsach]
+            print("Xóa thành công!!")
+            # with open("listbook.txt","r",encoding="utf8") as lbxs:
+            #     lbookxc = lbxs.readlines()
+            # lbxs.close()
+            # id = int(idsach)-1
+            # del lbookxc[id]
+            # l=open("listbook.txt","w+",encoding="utf8")
+            # for line in lbookxc:
+            #     l.write(line)
+            # l.close()
         else:
             print("ID sách không tìm thấy")
             
     
     def tim_sach_theo_id(self):
-        pass
+        idsach = input("Nhập ID sách: ")
+        if idsach in self.dictBook.keys():
+            print(f"Thông tin sách: {self.dictBook[idsach]['ten_sach']} - [{self.dictBook[idsach]['status']}] {self.dictBook[idsach]['nguoi_thue']} {self.dictBook[idsach]['ngay_thue']}")
+        else:
+            print("ID sách không tìm thấy")
     
     def tim_sach_theo_ten(self):
-        pass    
+        nhap_khoa = input("Nhập tên sách cần tìm: ")
+        for key,value in self.dictBook.items():
+            if value.get('ten_sach') == nhap_khoa:
+                print(key,"\t",value.get("ten_sach"),"- [",value.get("status"),"]")
+        else:    
+            print("Sách bạn tìm không thấy. Vui lòng kiểm tra lại khóa tìm kiếm!")
 
-    def sap_xep_sach_theo_ten(self):
-        pass
-
+    def sap_xep_sach_theo_id_giam_dan(self):
+        # print(sorted(self.dictBook.items(),reverse=True))
+        for key,value in sorted(self.dictBook.items(),reverse=True):
+            print(key,"\t",value.get("ten_sach"),"- [",value.get("status"),"]")
+        
     def kiem_tra_sach_da_thue(self):
-        pass
-
-
+        print("------------------------Danh sách sách đã thuê---------------------")
+        print("ID","\t", "Tên sách")
+        print("--------------------------------------------------------------------")
+        for key,value in self.dictBook.items():
+            if value.get('status') != "Có sẵn" :
+                print(key,"\t",value.get("ten_sach"),"- [",value.get("status"),"] - <",value.get("nguoi_thue"),"> - ",value.get("ngay_thue"))
+                
     def thue_sach(self):
         idsach = input("Nhập ID sách: ")
         current_day = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         if idsach in self.dictBook.keys():
             if not self.dictBook[idsach]['status'] == 'Có sẵn' :
                 print(f"Sách này đã được thuê bởi {self.dictBook[idsach]['nguoi_thue']} vào {self.dictBook[idsach]['ngay_thue']}")
-                return self.hien_thi()
             elif self.dictBook[idsach]['status'] == 'Có sẵn' :
                 nhap_ten = input("Nhập tên: ")
                 self.dictBook[idsach]['nguoi_thue'] = nhap_ten
@@ -126,15 +156,20 @@ class Library:
             print("ID sách không tìm thấy")
             
 
-
-
-
-
-if __name__ == "__main__":  
-    lbb = Library("listbook.txt")
-    lbb.hien_thi()
+# if __name__ == "__main__":  
+#     lbb = Library("listbook.txt")
+#     lbb.hien_thi()
     # lbb.them_sach()
-    lbb.thue_sach()
-    lbb.hien_thi()
-    lbb.tra_sach()
-    lbb.hien_thi()
+    # lbb.thue_sach()
+    # lbb.hien_thi()
+    # lbb.tra_sach()
+    # lbb.thue_sach()
+    # lbb.hien_thi()
+    # lbb.tim_sach_theo_id()
+    # lbb.tim_sach_theo_ten()
+    # lbb.sua_sach()
+    # lbb.hien_thi()
+    # lbb.kiem_tra_sach_da_thue()
+    # lbb.sap_xep_sach_theo_id_giam_dan()
+    # lbb.xoa_sach()
+    # lbb.hien_thi()
--- menu.py
+++ menu.py
@@ -1,5 +1,72 @@
-# with open("listbook.txt",encoding="utf8") as lb:
-#     lbook = lb.readlines()
+import library
 
+lbb = library.Library("listbook.txt") 
+
+while (True):
+    print("\n----------Chào mừng đến với thư viện AVU---------\n")
+    print(" 1. Danh sách sách thư viên \n 2. Thêm sách \n 3. Sửa sách",
+            " \n 4. Xóa sách \n 5. Tìm sách \n 6. Thuê sách \n 7. Trả sách \n 8. DS sách đã thuê \n 9.Thoát!")
+    try:
+        choice = int(input("Vui lòng chọn chức năng: "))
+    except ValueError:
+        print("Lỗi!\nXin hãy chọn số!")
+        
+    if choice == 1:
+        print("\nChức năng : Hiện thị danh sách sách thư viên\n")
+        lbb.hien_thi()
+    elif choice == 2:
+        print("\nChức năng : Thêm sách\n")
+        lbb.them_sach()
+    elif choice == 3:
+        print("\nChức năng : Cập nhật sách\n")
+        lbb.sua_sach()
+    elif choice == 4:
+        print("\nChức năng : Xóa sách\n")
+        lbb.xoa_sach()
+    elif choice == 5:
+        print("\nChức năng : Tìm kiếm sách\n",
+                "---1. Theo ID\n ---2. Theo tên sách\n")
+        try:
+            chon = int(input("Xin lựa chọn 1 trong 2 chức năng trên :"))
+        except ValueError:
+            print("Lỗi nhập!\nVui lòng làm lại")
+            break
+        else:
+            if chon == 1:
+                lbb.tim_sach_theo_id()
+            elif chon == 2:
+                lbb.tim_sach_theo_ten()
+            else:
+                print("Lựa chọn đó không tồn tại!")
+    elif choice == 6:
+        print("\nChức năng : Thuê sách\n")
+        lbb.thue_sach()
+    elif choice == 7:
+        print("\nChức năng : Trả sách\n")
+        lbb.tra_sach()
+    elif choice ==8:
+        print("\n Chức năng: DS sách đã thuê")
+        lbb.kiem_tra_sach_da_thue()
+    elif choice == 9:
+        print("\nThoát.\n")
+        break
+    else:
+        print("Chức năng chọn nằm ngoài hệ thống!")
+        continue
+
+   
+
+
+# with open("listbook.txt","r",encoding="utf8") as lb:
+#     lbook = lb.readlines()
+# lb.close()
+# s = input("Nhap")
+# l=open("listbook.txt","w",encoding="utf8")
 # for line in lbook:
-#     print(line)
+#     if line.strip("\n") != s:
+#         l.write(line)
+# s ="Có sẵn"
+# if s == "Có sẵn":
+#     print("true")
+# else:
+#     print("sai")
